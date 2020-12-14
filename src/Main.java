aabda0251aa1f2ccf6a36a506845dc7c99ff4a3d
Ma3tin <martin.mares@educanet.cz>
2020-12-14 10:22:22 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> pepa = new ArrayList<>();
+        Random r = new Random(100);
+        for (int i = 0; i < 100; i++) {
+            pepa.add(r.nextInt(100));
+        }
+        ArrayList<Integer> jirka = new ArrayList<>(pepa);
+        /*for (int i = 0; i < 50; i++) {
+            jirka.add(r.nextInt(100));
+        }*/
+        Collections.sort(pepa);
+        Collections.sort(jirka);
+        if (pepa.equals(jirka)) System.out.println("ANO");
+        else {
+            for (int i = 0; 50 < pepa.size(); i++) {
+                pepa.remove(i);
+            }
+        }
+
+        //System.out.println(pepa);
+        //System.out.println(jirka);
     }
 }
